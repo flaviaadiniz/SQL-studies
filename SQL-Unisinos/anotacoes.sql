784b9f1781d1b6acffd513776b32261d2df10a0c
flaviaadiniz <flaviaadiniz@gmail.com>
2023-09-09 17:11:37 -0300

    Adiçao de anotações sobre comandos SQL Oracle

---
+++ SQL-Unisinos/anotacoes.sql
@@ -0,0 +1,28 @@
+-- Cria uma Tabela
+-- CREATE TABLE nome_tabela(
+-- nome_atributo TIPO,
+-- nome_atributo TIPO
+-- );
+
+
+-- Remover a Tabela
+-- DROP TABLE nome_tabela;
+
+
+-- Adicionar Primary Key
+-- PRIMARY KEY(nome_atributo)
+
+
+-- Adicionar Foreign Key
+-- CONSTRAINT fk_nome_tabela_nome_atributo FOREIGN KEY (nome_atributo) REFERENCES nome_tabela (nome_atributo)
+
+
+-- Alterar uma Tabela
+-- ALTER TABLE nome_tabela
+-- ADD nome_atributo TIPO; (tem outras funcões ex deletar atributo, registrar primary key, etc)
+
+
+-- Inserir Dados em uma Tabela
+-- INSERT INTO nome_tabela VALUES (
+--     
+-- )
